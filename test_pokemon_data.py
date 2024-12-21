a3b2c3b59a326ad145be6d0344b0666e56c0eea4
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-12-20 23:51:00 -0600

    Pokemon Database Test
    A test for importing and reading the pokemon database files.

---
+++ test_pokemon_data.py
@@ -0,0 +1,13 @@
+# Project   : Pokedex AI
+# File      : Testing Pokemon Database
+# Author    : Tej Pandit
+# Date      : Nov 2024
+
+import os
+import pickle
+import yaml
+
+# Database Parameters
+src_yaml_folder = "./data/pokemon/yaml"
+dst_database_folder = "./data/pokemon"
+database_name = "pokemon.db"
