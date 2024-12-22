767cfbf9e0d7c22e337e576b7ea9194b26393bfd
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-12-21 23:38:56 -0600

    Load Pickled Pokemon Database
    Load the pickle file which has the pokemon db database.

--- test_pokemon_data.py
+++ test_pokemon_data.py
@@ -11,3 +11,8 @@ import yaml
 src_yaml_folder = "./data/pokemon/yaml"
 dst_database_folder = "./data/pokemon"
 database_name = "pokemon.db"
+
+# Load Pickle Dict Object (Database)
+file_path = os.path.join(dst_database_folder, database_name)
+with open(file_path, 'rb') as file:
+    pokemon = pickle.load(file)
