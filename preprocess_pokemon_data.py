0ed1ac9f59e3c38983cbaff7cd25809018d0d81a
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-12-22 21:28:53 -0600

    Data Preprocessing
    A script to process the YAML database into a more structured and compact database. Extract YAML function reads and loads a single YAML file.

---
+++ preprocess_pokemon_data.py
@@ -0,0 +1,17 @@
+# Project   : Pokedex AI
+# File      : Preprocessing YAML Pokemon Data
+# Author    : Tej Pandit
+# Date      : Nov 2024
+
+import os
+import pickle
+import yaml
+
+def extractYAML(file_path):
+    with open(file_path, 'r') as file:
+        try:
+            data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f"Error reading YAML file: {e}")
+            return None
