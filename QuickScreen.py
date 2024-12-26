3596c975a9d59b30ea8350d7bd48b71ac3ff0e30
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-12-25 23:01:00 -0600

    Quick Screen Class
    A mini-library for quickly grabbing screenshots of windows in python. Uses dxcam and pygetwindow libraries at the backend.

---
+++ QuickScreen.py
@@ -0,0 +1,14 @@
+# Project : Quick Screen Grabber
+# Author  : Tej Pandit
+# Date    : Nov 2024
+
+import dxcam
+import pygetwindow as pgw
+
+class QuickScreen:
+    def __init__(self):
+        self.cam = None
+        self.renderer = 0
+        self.monitor = 0
+        self.bbox = (0,0,100,100) # left, top, right, bottom
+        self.win = None
