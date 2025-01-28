65851bb26d8ce1537649836bfa4532c9a2308be8
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2025-01-27 23:16:35 -0600

    Augment Database TODO
    Added an example for reading database values and added a todo for the weakness augmentation.

--- augment_data.py
+++ augment_data.py
@@ -16,6 +16,11 @@ file_path = os.path.join(dst_database_folder, database_name)
 with open(file_path, 'rb') as file:
     pokemon = pickle.load(file)
 
+# Augment Database
+print(pokemon["pokemon-forms"]["bulbasaur"]["type1"])
+# TODO : Make "weaknesses" dict of all "type-chart" keys as keys, iterate through all super-effectives of all types and append them to the SET of values for each key
+# TODO : Add "weaknesses" to database under pokemon["type-chart"][type]["weak-to"]
+
 # Save as Augmented Database
 file_path = os.path.join(dst_database_folder, database_name)
 with open(file_path, 'wb') as file:
